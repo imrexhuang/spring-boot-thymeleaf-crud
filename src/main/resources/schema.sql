5d94042fdece05933dd72364d66686288fa44ca3
Rex <im@rex.tw>
2020-08-20 23:10:27 +0800

    增加中文註解

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,8 +2,8 @@
 
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book` (
-  `bookid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `author` varchar(255) DEFAULT NULL,
+  `bookid` int(11) NOT NULL AUTO_INCREMENT COMMENT '編號',
+  `name` varchar(255) DEFAULT NULL COMMENT '書籍名稱',
+  `author` varchar(255) DEFAULT NULL COMMENT '作者',
   PRIMARY KEY (`bookid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
