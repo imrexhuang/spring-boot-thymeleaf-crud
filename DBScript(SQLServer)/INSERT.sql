560445f7d553e1e1b99f57b1522ea2e98dd26f93
Rex <im@rex.tw>
2020-08-24 22:13:52 +0800

    SET IDENTITY_INSERT

--- DBScript(SQLServer)/INSERT.sql
+++ DBScript(SQLServer)/INSERT.sql
@@ -1,3 +1,4 @@
+SET IDENTITY_INSERT [dbo].[book] ON
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (1, 'Test1', 'ABCDE1');
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (2, '異境之書：安伯托‧艾可最後人文藝術巨作', '安伯托．艾可');
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (3, '窮查理的投資哲學與選股金律', '崔恩．葛瑞芬');
@@ -11,3 +12,4 @@ INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (10, '被討厭的�
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (11, '魔法精油寶典：102種植物香氣的能量運用', '史考特‧康寧罕');
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (12, '天生變態：一個擁有變態大腦的天才科學家', '詹姆斯‧法隆');
 INSERT INTO [dbo].[book] ([bookid], [name], [author]) VALUES (13, '億男', '川村元氣');
+SET IDENTITY_INSERT [dbo].[book] OFF
